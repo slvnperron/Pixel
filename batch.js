7e9d528f1b4cd5ef568ba4c68d917a417657745f
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 02:58:41 -0400

    added logging

--- batch.js
+++ batch.js
@@ -2,8 +2,19 @@ var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 var numpad = require('numpad');
 var nrc = 0;
-var child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc' + argv.nrc);
+var child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc' + argv.nrc, function(err, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+	  console.log('exec error: ' + error);
+	}	
+});
+
 console.log("Ran with nrc:", numpad(argv.nrc, 5));
+
+child.on('', function() {
+	
+});
 
 child.on('exit', function() {
 	console.log("Finished.");
