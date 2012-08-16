ccb23defa40fed689af6ec24f8b5a5e5660cb0a9
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 03:07:45 -0400

    forgot numpad

--- batch.js
+++ batch.js
@@ -12,8 +12,8 @@ if(argv.nrc == 99999) {
 console.log("Ran with nrc:", numpad(argv.nrc, 5));
 
 var execNext = function() {
-	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + argv.nrc++, function(err, stdout, stderr) {
-		console.log('NRC#', argv.nrc);
+	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + numpad(argv.nrc++, 5), function(err, stdout, stderr) {
+		console.log('NRC#', numpad(argv.nrc, 5));
 		if (err !== null) {
 		  console.log('Output: ' + error);
 		}
