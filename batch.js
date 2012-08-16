f5530ecf4326903d297155d8b5497717ab3cb9fd
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 02:46:05 -0400

    Batching support

---
+++ batch.js
@@ -0,0 +1,11 @@
+var exec = require('child_process').exec;
+var argv = require('optimist').argv;
+var numpad = require('numpad');
+var nrc = 0;
+var child = exec('node', ['spider.js', ['--u SYPER54', '-p b1idyq54', '--session 201001', '--nrc ' + argv.nrc]]);
+
+console.log("Ran with nrc:", argv.nrc);
+
+child.on('exit', function() {
+	exec('', ['-nrc' + numpad(nrc+1, 5)]);
+});
