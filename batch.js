54200220117a24787ab52ec054bd34bbd473c7ac
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 02:55:44 -0400

    Fixed exec arguments

--- batch.js
+++ batch.js
@@ -2,10 +2,9 @@ var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 var numpad = require('numpad');
 var nrc = 0;
-var child = exec('node', ['spider.js', ['--u SYPER54', '-p b1idyq54', '--session 201001', '--nrc ' + argv.nrc]]);
-
+var child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc' + argv.nrc);
 console.log("Ran with nrc:", argv.nrc);
 
 child.on('exit', function() {
-	exec('', ['-nrc' + numpad(nrc+1, 5)]);
+	exec('node batch.js --nrc' + numpad(argv.nrc, 5));
 });
