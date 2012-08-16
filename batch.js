a798110678877e9c7de037e1d32c49b887ae1734
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 02:56:44 -0400

    numpad argv

--- batch.js
+++ batch.js
@@ -3,8 +3,9 @@ var argv = require('optimist').argv;
 var numpad = require('numpad');
 var nrc = 0;
 var child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc' + argv.nrc);
-console.log("Ran with nrc:", argv.nrc);
+console.log("Ran with nrc:", numpad(argv.nrc, 5));
 
 child.on('exit', function() {
-	exec('node batch.js --nrc' + numpad(argv.nrc, 5));
+	console.log("Finished.");
+	exec('node batch --nrc' + numpad(argv.nrc, 5));
 });
