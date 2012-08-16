81732940392f8971a39125c146d48b102312c77b
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 03:11:45 -0400

    modified session for testing purpose

--- batch.js
+++ batch.js
@@ -12,7 +12,7 @@ if(argv.nrc == 99999) {
 console.log("Ran with nrc:", numpad(argv.nrc, 5));
 
 var execNext = function() {
-	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + numpad(argv.nrc++, 5), function(err, stdout, stderr) {
+	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201109 --nrc ' + numpad(argv.nrc++, 5), function(err, stdout, stderr) {
 		console.log('NRC#', numpad(argv.nrc, 5));
 		if (err !== null) {
 		  console.log('Output: ' + error);
