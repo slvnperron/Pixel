c1f61b4fd81301d0c232f37e246e4762509cde51
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 03:02:09 -0400

    refactor

--- batch.js
+++ batch.js
@@ -2,17 +2,22 @@ var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 var numpad = require('numpad');
 var nrc = 0;
-var child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + argv.nrc, function(err, stdout, stderr) {
-	console.log('stdout: ' + stdout);
-	console.log('stderr: ' + stderr);
-	if (err !== null) {
-	  console.log('exec error: ' + error);
-	}	
-});
+var child;
+execNext();
 
 console.log("Ran with nrc:", numpad(argv.nrc, 5));
 
 child.on('exit', function() {
 	console.log("Finished.");
-	exec('node batch --nrc ' + numpad(argv.nrc, 5));
+	execNext();
 });
+
+var execNext = function() {
+	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + argv.nrc++, function(err, stdout, stderr) {
+		console.log('stdout: ' + stdout);
+		console.log('stderr: ' + stderr);
+		if (err !== null) {
+		  console.log('exec error: ' + error);
+		}	
+	});
+};
