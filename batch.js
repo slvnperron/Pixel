ff1e6b58e42930ae94358f8ef01f733f8823ee0e
Sylvain Perron <slvn.perron@gmail.com>
2012-08-16 03:03:42 -0400

    infinite loop

--- batch.js
+++ batch.js
@@ -6,18 +6,18 @@ var child;
 
 console.log("Ran with nrc:", numpad(argv.nrc, 5));
 
-child.on('exit', function() {
-	console.log("Finished.");
-	execNext();
-});
-
 var execNext = function() {
 	child = exec('node spider.js --u SYPER54 --p b1idyq54 --session 201001 --nrc ' + argv.nrc++, function(err, stdout, stderr) {
 		console.log('stdout: ' + stdout);
 		console.log('stderr: ' + stderr);
 		if (err !== null) {
 		  console.log('exec error: ' + error);
-		}	
+		}
+	});
+	
+	child.on('exit', function() {
+		console.log("Finished.");
+		execNext();
 	});
 };
 
