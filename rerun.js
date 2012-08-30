e2bda088cc781f7762da55aadb5288ff65d1d9aa
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 11:46:47 -0400

    sdfsdf

--- rerun.js
+++ rerun.js
@@ -5,6 +5,30 @@ var databaseUrl = "127.0.0.1/pixel"; // "username:password@example.com/mydb"
 var collections = ["courses"]
 var db = require("mongojs").connect(databaseUrl, collections);
 
+var nrcs = [];
+var index = 0;
+
 db.courses.find(function(err, docs) {
 	console.log("All courses (" + docs.length + ")");
+
+	nrcs = docs;
+	index = 0;
+
 });
+
+var execNext = function() {
+	child = exec('node spider.js --u ' + argv.u + ' --p ' + argv.p + ' --s ' + nrcs[index].session + ' --nrc ' + numpad(nrcs[index].nrc, 5), function(err, stdout, stderr) {
+		console.log('NRC#', numpad(argv.nrc, 5));
+		console.log('stdout:', stdout);
+		console.log('stderr:', stderr)
+		if (err !== null) {
+		  console.log('Output: ' + error);
+		}
+	});
+	
+	child.on('exit', function() {
+		index++;
+		execNext();
+		}
+	});
+};
