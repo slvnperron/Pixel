4db8f689ee3c4e0358c900e7cf0f9fe7dbb90632
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 02:37:04 -0400

    allow cross domain request

--- web/app.js
+++ web/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.post('/query', function(req, res){
-  res.send('hello world');
+app.post('/query', function(req, res) {
+	
+	res.writeHead(200, {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*"
 });
+
+    res.send('Username: ');
+};
 
 app.listen(8080);
