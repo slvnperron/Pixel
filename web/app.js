84198b3aab92731d080725e89c3c223b06919084
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 02:42:28 -0400

    CORS

--- web/app.js
+++ web/app.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', config.allowedDomains);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
-}
-
-app.configure(function() {
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.methodOverride());
-    app.use(allowCrossDomain);
-    app.use(app.router);
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
 app.post('/query', function(req, res) {
