077b44701eef04045e36563bbde5be1da191aa82
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 04:58:27 -0400

    queries

--- web/app.js
+++ web/app.js
@@ -16,15 +16,38 @@ app.use(express.bodyParser());
 app.post('/title', function(req, res) {
 	console.log(req.query);
 
+	var queryObj = {};
 
 	var regNrc = /NRC:\s"(.*?)"/i;
-	regNrc.exec(req.query.title);
-	console.log("NRC : " + RegExp.$1);
-	console.log(regNrc.test(req.query.title));
+	var regTitle = /Titre:\s"(.*?)"/i;
+	var regProfesseur = /Professeur:\s"(.*?)"/i;
+	var regNum = /Numéro:\s"(.*?)"/i;
 
+	if(regNrc.test(req.query.title)) {
+		regNrc.exec(req.query.title);
+		queryObj.nrc = new RegExp(RegExp.$1, 'i');
+		console.log("NRC " + RegExp.$1 + " taken.");
+	}
 
+	if(regTitle.test(req.query.title)) {
+		regTitle.exec(req.query.title);
+		queryObj.title = new RegExp(RegExp.$1, 'i');
+		console.log("Titre " + RegExp.$1 + " taken.");
+	}
 
-	db.courses.find({title: new RegExp(req.query.title, 'i')}).limit(25, function(err, data) {
+	if(regProfesseur.test(req.query.title)) {
+		regProfesseur.exec(req.query.title);
+		queryObj.teachers = new RegExp(RegExp.$1, 'i');
+		console.log("Professeur " + RegExp.$1 + " taken.");
+	}
+
+	if(regNum.test(req.query.title)) {
+		regNum.exec(req.query.title);
+		queryObj.number = new RegExp(RegExp.$1, 'i');
+		console.log("Numéro " + RegExp.$1 + " taken.");
+	}
+
+	db.courses.find(queryObj).limit(25, function(err, data) {
     res.json(data);
 	});
 });
