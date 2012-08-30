3faf91988df50fb5f8f36faa65d2366ce1842f9a
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 02:37:59 -0400

    fix

--- web/app.js
+++ web/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 
 app.post('/query', function(req, res) {
-	
+
 	res.writeHead(200, {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*"
 });
 
     res.send('Username: ');
-};
+});
 
 app.listen(8080);
