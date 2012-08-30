6cb9afb57db603477f5beb627f713c3035834077
Sylvain Perron <slvn.perron@gmail.com>
2012-08-30 11:41:13 -0400

    fsdfdsf

--- rerun.js
+++ rerun.js
@@ -1,16 +1,15 @@
 var exec = require('child_process').exec;
 var argv = require('optimist').argv;
 var numpad = require('numpad');
-var nrc = 0;
-var child;
-if(argv.c == undefined) argv.c = 1;
-
 var databaseUrl = "127.0.0.1/pixel"; // "username:password@example.com/mydb"
 var collections = ["courses"]
 var db = require("mongojs").connect(databaseUrl, collections);
 
-db.courses.find(function(err, co) {
-	console.log("All courses (" + co.length + ")");
+var nrc = 0;
+var child;
+
+db.courses.find(function(err, docs) {
+	console.log("All courses (" + docs.length + ")");
 });
 
 var execNext = function() {
